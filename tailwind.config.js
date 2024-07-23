ddf7dfe3d3097dc2794f55cc581a94d3e48ecd48
Divyansh Khare <dkhare@ucsc.edu>
2024-07-23 15:08:07 -0500

    Tailwind Setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,jsx,js}", "./src/**/*.{html,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
