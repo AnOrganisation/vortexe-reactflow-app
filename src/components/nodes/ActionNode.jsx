cbace10518cfee16e785121750a01cfd8ee15c3f
Divyansh Khare <dkhare@ucsc.edu>
2024-08-03 18:05:47 -0500

    ActionNode UI Complete

--- src/components/nodes/ActionNode.jsx
+++ src/components/nodes/ActionNode.jsx
@@ -1,50 +1,33 @@
 import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-  Divider,
-  Link,
-  Image,
-} from "@nextui-org/react";
-import { useCallback, useState } from "react";
+import { Chip, Badge, Button } from "@nextui-org/react";
+import { useCallback, useState, useRef } from "react";
 import { Handle, Position } from "@xyflow/react";
 
 function ActionNode({ id, data }) {
   // FileNode implementation goes here
+
+  //State to track the delete button press
+  const [isDeleteButtonPressed, setIsDeleteButtonPressed] = useState(false);
+
+  const handleDeleteButtonClick = () => {
+    console.log("Delete button clicked", id);
+  };
+
   return (
-    <Card className="max-w-[400px]">
-      <CardHeader className="flex gap-3">
-        <Image
-          alt="nextui logo"
-          height={40}
-          radius="sm"
-          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-          width={40}
-        />
-        <div className="flex flex-col">
-          <p className="text-md">{data.label}</p>
-          <p className="text-small text-default-500">nextui.org</p>
-        </div>
-      </CardHeader>
-      <Divider />
-      <CardBody>
-        <p>Make beautiful websites regardless of your design experience.</p>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <Link
-          isExternal
-          showAnchorIcon
-          href="https://github.com/nextui-org/nextui"
-        >
-          Visit source code on GitHub.
-        </Link>
-      </CardFooter>
-      <Handle type="target" position={Position.Left} />
-      <Handle type="source" position={Position.Right} />
-    </Card>
+    <>
+      <Badge
+        content="-"
+        color="danger"
+        onClick={handleDeleteButtonClick}
+        className="opacity-100 cursor-pointer hover:opacity-60"
+      >
+        <Chip size="lg" color="warning" variant="shadow">
+          {data.label}
+        </Chip>
+      </Badge>
+      <Handle type="target" position={Position.Top} />
+      <Handle type="source" position={Position.Bottom} />
+    </>
   );
 }
 
